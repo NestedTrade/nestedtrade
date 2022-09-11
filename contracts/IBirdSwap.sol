57a2ae2b12c19983642cbeac764d186fe743d393
fabdarice <fabrice.cheng@gmail.com>
2022-09-11 16:58:46 -0700

    add indexed events + refactor code + add happy case tests

--- contracts/IBirdSwap.sol
+++ contracts/IBirdSwap.sol
@@ -18,28 +18,54 @@ interface IBirdSwap {
 
     /// @notice Emitted when an ask is created
     /// @param tokenId The ERC-721 token ID of the created ask
-    /// @param ask The metadata of the created ask
-    event AskCreated(uint256 indexed tokenId, Ask ask);
+    event AskCreated(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 askPrice,
+        uint256 royaltyFeeBps,
+        bytes32 uid
+    );
 
     /// @notice Emitted when an ask price is updated
     /// @param tokenId The ERC-721 token ID of the updated ask
-    /// @param ask The metadata of the updated ask
-    event AskPriceUpdated(uint256 indexed tokenId, Ask ask);
+    event AskPriceUpdated(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 askPrice,
+        uint256 royaltyFeeBps,
+        bytes32 uid
+    );
 
     /// @notice Emitted when an ask is canceled
     /// @param tokenId The ERC-721 token ID of the canceled ask
-    /// @param ask The metadata of the canceled ask
-    event AskCanceled(uint256 indexed tokenId, Ask ask);
+    event AskCanceled(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 askPrice,
+        uint256 royaltyFeeBps,
+        bytes32 uid
+    );
 
     /// @notice Emitted when an ask is filled
     /// @param tokenId The ERC-721 token ID of the filled ask
-    /// @param buyer The buyer address of the filled ask
-    /// @param ask The metadata of the filled ask
-    event AskFilled(uint256 indexed tokenId, address indexed buyer, Ask ask);
+    event AskFilled(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 askPrice,
+        uint256 royaltyFeeBps,
+        bytes32 uid
+    );
 
     /// @notice Emitted when an bird is withdrawn without a sale by the original owner
     /// @param tokenId The ERC-721 token ID of the filled ask
     /// @param to The address the bird was withdrawn to
-    event BirdWithdrawn(uint256 indexed tokenId, address to);
+    event BirdWithdrawn(
+        uint256 indexed tokenId,
+        address indexed to
+   );
 
 }
---
+++ test/Birdswap/test_fill_ask.js
@@ -0,0 +1,54 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+const { deployBirdswap } = require("../helpers/deploy_everything");
+
+describe("BirdSwap", () => {
+  let birdswap, moonbirds;
+  let feePayout = "0x47A90D927DfA99EC3a3582D2C4DAbf12cF58f340";
+  let feeBps = 200; // 2%
+
+  let deployer, minterA, minterB, buyer, buyer2;
+
+  let askPrice = ethers.utils.parseEther("10");
+  let royaltyBps = 0; // 0% to MoonBirds
+
+  let tokenId;
+
+  beforeEach(async () => {
+    [deployer, minterA, minterB, buyer, buyer2] = await ethers.getSigners();
+
+    [birdswap, moonbirds] = await deployBirdswap(feePayout, feeBps);
+
+    await moonbirds.mintUnclaimed(minterA.address, 2);
+    await moonbirds.mintUnclaimed(minterB.address, 2);
+    await moonbirds.connect(minterA).toggleNesting([0, 1]);
+    await moonbirds.connect(minterB).toggleNesting([2]);
+
+    tokenId = 0;
+    // Create Ask
+    await birdswap
+      .connect(minterA)
+      .createAsk(tokenId, buyer.address, askPrice, royaltyBps);
+
+    // Send MB to Contracts
+    await moonbirds.connect(minterA).safeTransferWhileNesting(minterA.address, birdswap.address, tokenId);
+  });
+
+  describe("BirdSwap.fillAsk (Success)", async () => {
+    it("fill the ask", async () => {
+
+      const initialBalance = await birdswap.provider.getBalance(minterA.address);
+
+      await birdswap.connect(buyer).fillAsk(tokenId, {value: askPrice});
+
+      expect(await moonbirds.ownerOf(tokenId)).equals(buyer.address);
+      expect(await birdswap.moonbirdTransferredFromOwner(tokenId)).equals(ethers.constants.AddressZero);
+      // Check ETH Balance
+      expect(await birdswap.provider.getBalance(minterA.address)).equals(ethers.utils.parseEther("9.8").add(initialBalance))
+    });
+
+  })
+});
+
+
