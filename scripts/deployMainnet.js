882a001cbe81d227add09fe6d1650e2f462fe610
fabdarice <fabrice.cheng@gmail.com>
2022-09-22 01:51:47 -0400

    updaet timelock

--- scripts/deployMainnet.js
+++ scripts/deployMainnet.js
@@ -1,8 +1,8 @@
 const { ethers } = require("hardhat");
 
-const MULTISIG= "0x08A775ed86A563a77753727a1dbbcb29995531A1";  // GnosisSafe
+const MULTISIG = "0x08A775ed86A563a77753727a1dbbcb29995531A1";  // GnosisSafe
 const MB_ADDRESS = "0x23581767a106ae21c074b2276D25e5C3e136a68b"; // MoonBird NFT
-const TIMELOCK = "0xF58129Ed3404f63CEC0d441b838dE65814D2470E"; // Timelock
+const TIMELOCK = "0xc31647f37FCEf38cD02C930245b5adcF90e46159"; // Timelock
 const FEE_BPS = 200; // 2%
 
 async function main() {
